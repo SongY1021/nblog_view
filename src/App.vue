8a55a84ee363c806ab29a6c8d1015a4a0093b26f
songyang <dafy@songyang>
2019-07-25 10:51:57 +0800

    背景

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,13 @@ export default {
     background-image: url(//img.alicdn.com/tps/TB1d.u8MXXXXXXuXFXXXXXXXXXX-1900-790.jpg);
     background-size: 100%;
     /*! autoprefixer: off */
-    background-image: -webkit-gradient(linear, 0 0, 100% 100%, color-stop(0, #003073), color-stop(100%, #029797));
-    /*background-image: -webkit-linear-gradient(135deg, #003073, #029797);*/
+    background: -webkit-gradient(linear, 0 0, 100% 100%, color-stop(0, #003073), color-stop(100%, #029797));
+    /*background: -webkit-linear-gradient(135deg, #003073, #029797);*/
     /* autoprefixer: on */
-    background-image: -moz-linear-gradient(45deg, #003073, #029797);
-    background-image: -ms-linear-gradient(45deg, #003073 0, #029797 100%);
-    background-image: -o-linear-gradient(45deg, #003073, #029797);
-    /*background-image: linear-gradient(135deg, #003073, #029797);*/
+    background: -moz-linear-gradient(45deg, #003073, #029797);
+    background: -ms-linear-gradient(45deg, #003073 0, #029797 100%);
+    background: -o-linear-gradient(45deg, #003073, #029797);
+    /*background: linear-gradient(135deg, #003073, #029797);*/
     text-align: center;
     margin: 0px;
     overflow: hidden;
